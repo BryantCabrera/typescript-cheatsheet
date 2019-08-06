089ca6e7da5d0eb2a01bbc625428eb604316c304
Bryant Cabrera <bcabrera@gocurrency.com>
2019-08-06 14:37:33 -0700

    Bryant creates variable annotation examples.

---
+++ annotations/variables.ts
@@ -0,0 +1,12 @@
+// Tells TypeScript that we are only ever going to assign a value of type number to variable apples
+let apples: number = 5;
+let speed: string = 'fast';
+// let speed: string = 0 // => will give a linting error
+let hasName: boolean = true;
+// be aware of when you're working with something that is a type vs. a value
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
