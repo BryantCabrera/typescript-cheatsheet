44443185964a0548c6f2adb772b128435d5fe1c3
Bryant Cabrera <bcabrera@gocurrency.com>
2019-08-06 14:39:49 -0700

    Bryant creates variable array annotation examples.

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -10,3 +10,7 @@ let nothing: undefined = undefined;
 // built in objects
 let now: Date = new Date();
 
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
