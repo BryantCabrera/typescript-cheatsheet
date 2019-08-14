43350d7794518586ac551d252bec9c853b59fa70
Bryant Cabrera <bcabrera@gocurrency.com>
2019-08-14 11:08:35 -0700

    Bryant adds other examples of when to use annotations over inferences.

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -36,3 +36,31 @@ let point: { x: number; y: number; } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+
+// When to use Annotations
+  // 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+  // Whenever we call JSON.parse();, we get back a value of the any type
+  // SOLUTION: add typing
+const coordinates: { x: number; y: number}  = JSON.parse(json);
+console.log(coordinates); // {x:10, y: 20};
+
+  // 2) When we declare a variable on 1 line & intialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean; // best way: let foundWord = false; so TypeScript can infer type
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+  // 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
